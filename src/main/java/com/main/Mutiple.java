4e4b279b1c3c2858998bb364c6ed655b36626198
dwzd <dwzd@users.noreply.github.com>
2018-05-05 21:24:00 -0400

    Data Driven Framework made jar file

---
+++ src/main/java/com/main/Mutiple.java
@@ -0,0 +1,15 @@
+package com.main;
+
+public class Mutiple {
+    public static void main(String[] args) {
+        simpleMultiple(9);
+    }
+    public static void simpleMultiple(int num){
+        for(int x=1; x<=num; x++){
+            for(int y=1; y<=x; y++){
+                System.out.print(y + "*" + x + "=" + y*x +"\t");
+            }
+            System.out.println();
+        }
+    }
+}
